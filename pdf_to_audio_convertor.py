87d20dd232f87f7bcd380f8e1879e5f67a0ba635
zeeshanravian1 <62133352+zeeshanravian1@users.noreply.github.com>
2022-10-24 16:26:20 +0500

    Update pdf_to_audio_convertor.py

--- pdf_to_audio_convertor.py
+++ pdf_to_audio_convertor.py
@@ -9,10 +9,15 @@ from PyPDF4 import PdfFileReader
 from gtts import gTTS
 
 def pdf_to_audio(file_name):
+    """
+    The code will covert the text of pdf into the audio.
+    It will save the audio file with mp3 extension.
+    """
     pdf = PdfFileReader(file_name)
     for page in range(pdf.getNumPages()):
         text = pdf.getPage(page).extractText()
         tts = gTTS(text=text, lang='en')
         tts.save(f"{page}.mp3")
 
+# Function Calling
 pdf_to_audio("File.pdf")
